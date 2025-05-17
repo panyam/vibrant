29c0f6acca48eb68ff1f5072245a5a613603bd46
Sri Panyam <sri.panyam@gmail.com>
2025-05-17 14:36:42 -0700

    Adding sample tools

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
 
 	gotl "github.com/panyam/goutils/template"
@@ -11,6 +13,10 @@ func main() {
 	nargs := len(os.Args)
 	if nargs > 1 && os.Args[1] == "tools" {
 		if nargs > 2 {
+			if os.Args[2] == "json" {
+				ToolsJson()
+				return
+			}
 			if os.Args[2] == "run" {
 				RunTool(os.Args[3], os.Args[4:])
 				return
@@ -27,4 +33,56 @@ func main() {
 		},
 	}
 	b.Serve(nil, ":7777")
+}
+
+var tools map[string]Tool
+
+func init() {
+	tools = map[string]Tool{
+		"read_file":  &ReadFile{ProjectRoot: "./"},
+		"list_files": &ListFiles{ProjectRoot: "./"},
+	}
+}
+
+func RunTool(name string, args []string) {
+	tools[name].Run(args)
+}
+
+func ToolsJson() {
+	var out []any
+	for _, tool := range tools {
+		props := map[string]any{}
+		for _, param := range tool.Parameters() {
+			props[param.Name] = param.Json()
+		}
+		out = append(out, map[string]any{
+			"name":        tool.Name(),
+			"description": tool.Description(),
+			"parameters":  map[string]any{"type": "object", "properties": props},
+		})
+	}
+
+	b, err := json.Marshal(out)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("JSON: ")
+	fmt.Println(string(b))
+}
+
+func PrintTools() {
+	fmt.Println("Tools:")
+	for name, tool := range tools {
+		fmt.Printf("	%s: \n", name)
+		fmt.Println(tool.Description())
+		fmt.Println("	Parameters:")
+		for _, param := range tool.Parameters() {
+			fmt.Printf("		%s (%s) - %s\n", param.Name, param.Type, param.Description)
+		}
+		fmt.Println("	Returns:")
+		for _, param := range tool.Returns() {
+			fmt.Printf("		%s (%s) - %s\n", param.Name, param.Type, param.Description)
+		}
+		fmt.Println("===============================")
+	}
 }
--- tools.go
+++ tools.go
@@ -1,36 +1,62 @@
 package main
 
+import (
+	"os"
+	"path/filepath"
+)
+
 type Parameter struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
 	Type        string `json:"type"`
 }
 
-type Tool struct {
-	Name        string      `json:"name"`
-	Description string      `json:"description"`
-	Parameters  []Parameter `json:"input_params"`
-	Returns     []Parameter `json:"output_param"`
+func (p *Parameter) Json() any {
+	return map[string]any{
+		"description": p.Description,
+		"type":        p.Type,
+	}
 }
 
-var ReadFile = Tool{
-	Name: "read_file",
-	Description: `
+type Tool interface {
+	Name() string
+	Description() string
+	Parameters() []*Parameter
+	Returns() []*Parameter
+	Run(args []string) (any, error)
+}
+
+type ReadFile struct {
+	ProjectRoot string
+}
+
+func (r *ReadFile) Name() string {
+	return "read_file"
+}
+
+func (r *ReadFile) Description() string {
+	return `
 	Reads a file with the path given in the 'path' parameter.  The path of the file is ALWAYS relative to the project root.
 
 	Returns 2 values as json:
 
 	content: The content bytes of the file
 	metadata: Metadata of the file as a json dictionary.
-	`,
-	Parameters: []Parameter{
+	`
+}
+
+func (r *ReadFile) Parameters() []*Parameter {
+	return []*Parameter{
 		{
 			Name:        "path",
 			Description: "Path of the file to read contents for.  Path will be resolved to a relative path in the current project",
 			Type:        "string",
 		},
-	},
-	Returns: []Parameter{
+	}
+}
+
+func (r *ReadFile) Returns() []*Parameter {
+	return []*Parameter{
 		{
 			Name:        "contents",
 			Description: "Contents of the file if it exists.",
@@ -41,11 +67,101 @@ var ReadFile = Tool{
 			Description: "If the file does not exist or cannot be read then an error is returned",
 			Type:        "string",
 		},
-	},
+	}
 }
 
-func RunTool(name string, args []string) {
+func (r *ReadFile) Run(args []string) (any, error) {
+	path := args[0]
+	fullpath, err := filepath.Abs(filepath.Join(r.ProjectRoot, path))
+	if err != nil {
+		return nil, err
+	}
+
+	content, err := os.ReadFile(fullpath)
+	if err != nil {
+		return nil, err
+	}
+	return string(content), nil
 }
 
-func PrintTools() {
+type ListFiles struct {
+	ProjectRoot string
+}
+
+func (r *ListFiles) Name() string {
+	return "list_files"
+}
+
+func (r *ListFiles) Description() string {
+	return `
+	Lists the files in a given folder provided by the 'path' parameter.   The 'recurse' parameter enables recursive file listing.
+
+	Returns an error or a list of entries.  Each entry is a file or a folder.   Folder entries could recursively contain other entries.
+	`
+}
+
+func (r *ListFiles) Parameters() []*Parameter {
+	return []*Parameter{
+		{
+			Name:        "path",
+			Description: "Path of the folder to list files in.  Path will be resolved to a relative path in the current project",
+			Type:        "string",
+		},
+		{
+			Name:        "recurse",
+			Description: "Whether to return files recursively.  This will help with minimizing number of tool calls if the folder content changes (file creations and deletions) are minimal",
+			Type:        "boolean",
+		},
+	}
+}
+
+func (r *ListFiles) Returns() []*Parameter {
+	return []*Parameter{
+		{
+			Name:        "entries",
+			Description: "List of entries.  Each entry can be a folder or a file.  If the entry is a folder, then it may recursively contain entries - if the recurse parameter was set to true",
+			Type:        "object",
+		},
+	}
+}
+
+func (r *ListFiles) Run(args []string) (any, error) {
+	path := args[0]
+	dir, err := filepath.Abs(filepath.Join(r.ProjectRoot, path))
+	if err != nil {
+		return nil, err
+	}
+
+	var entries []any
+	err = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		relPath, err := filepath.Rel(dir, path)
+		if err != nil {
+			return err
+		}
+
+		entry := map[string]any{}
+		if relPath != "." {
+			if info.IsDir() {
+				entry = map[string]any{
+					"is_folder": true,
+				}
+			} else {
+				entry = map[string]any{
+					"path": relPath,
+				}
+			}
+			entries = append(entries, entry)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return entries, err
 }
